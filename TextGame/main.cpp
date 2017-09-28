9c269447f9d7d95a15be36d3700e1eb473f9b0a3
Trinity Krile <trinitykrile@Trinitys-MacBook-Air.local>
2017-09-28 14:53:57 -0400

    Initial Commit

---
+++ TextGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TextGame
+//
+//  Created by Trinity Krile on 9/28/17.
+//  Copyright © 2017 Trinity Krile. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
